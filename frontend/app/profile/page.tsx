c4032db770b341c502a122fcc72c06ac7eede852
KaitoTakei0618 <kaitonkaiton2005@gmail.com>
2026-06-16 14:43:23 +0900

    profile Webサイトを開発

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+export default function ProfilePage() {
+  return (
+    <div>
+      <h1>自己紹介</h1>
+
+      <Image
+        src="/profile.png"
+        alt="プロフィール画像"
+        width={100}
+        height={100}
+      />
+
+      <p>武井 海渡</p>
+        <p>
+          <strong>所属：</strong>
+          法政大学
+        </p>
+
+        <p>
+          <strong>研究分野：</strong>
+          生成AI
+        </p>
+
+        <p>
+          <strong>趣味：</strong>
+          猫
+        </p>
+      </div>
+      
+  
+  );
+}
